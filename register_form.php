c22369da9baa83bcc8e57ac1bba57169d18d9e27
Minkcode <kvedaras.mindaugas@gmail.com>
2023-02-20 00:22:51 +0200

    reformated register_form.php html part

--- register_form.php
+++ register_form.php
@@ -57,126 +57,177 @@ if (isset($_POST['signin'])) {
 </head>
 
 <body>
-    <section class="hero is-fullheight">
-        <div class="hero-body">
-            <div class="container is-widescreen">
-                <div class="columns is-centered is-tablet">
-                <div class="notification is-success has-text-centered" id="success-message" style="display:none; max-height:80px;">
-                    <p>Registracija sėkminga! Netrukus galėsite prisijungti.</p>
-                </div>
-                    <div class="column is-one-third" id="form-container" style="min-width:450px;">
-                        <div class="container mb-4 has-text-centered">
-                            <h1 class="title has-text-success" id="form-title">PRISIJUNGIMAS</h1>
-                        </div>
-                        <div class="container is-flex-touch is-widescreen">
-                            <?php
-                            if (isset($errors['match'])) {
-                                echo '<span class="error-msg">' . $errors['match'] . '</span>';
-                            };
-                            ?>
-                            <form style="width: 100%;" id="signin-form" action="" method="post">
-                                <div class="field">
-                                    <label class="label">El. paštas</label>
-                                    <div class="control">
-                                        <input class="input is-medium" type="email" name="email" placeholder="Jūsų el. paštas" value="<?php echo isset($_POST['email']) ? $_POST['email'] : '' ?>">
-                                    </div>
-                                    <?php
-                                        if (isset($errors['email'])) { ?>
-                                            <span id="email-error" class="has-text-danger"><?php echo $errors['email'] ?></span>
-                                    <?php } ?>
-                                </div>
-                                <div class="field">
-                                    <label class="label">Slaptažodis</label>
-                                    <div class="control">
-                                        <input class="input is-medium" type="password" name="password" placeholder="Slaptažodis" value="<?php echo isset($_POST['password']) ? $_POST['password'] : '' ?>">
-                                    </div>
-                                </div>
-                                <div class="container has-text-centered">
-                                    <?php
-                                        if (isset($errors['signin'])) { ?>
-                                            <span id="signin-error" class="has-text-danger"><?php echo $errors['signin'] ?> </span>
-                                    <?php } ?>
-                                </div>
-                                <div class="field has-text-centered">
-                                    <input 
-                                        id="signin" 
-                                        class="button is-success is-fullwidth" 
-                                        type="submit" name="signin" 
-                                        value="Prisijungti"
-                                    >
-                                    <p>Neturite paskyros?</p>
-                                    <p class="has-text-success pointer" id="signup-button">Užsiregistruokite</p>
-                                </div>
-                            </form>
-                            <form style="width:100%; display:none;" id="signup-form" action="register_form.php" method="post">
-                                <div class="field">
-                                    <label class="label">Vardas</label>
-                                    <div class="control is-expanded">
-                                        <input class="input is-medium <?php echo isset($errors['name']) ? 'is-danger' : '' ?>" type="text" name="name" id="name" placeholder="Jūsų vardas" value="<?php echo isset($_POST['name']) ? $_POST['name'] : '' ?>">
-                                    </div>
-                                    <p id="name-error" class="help is-danger error-message"></p>
-                                </div>
-                                <div class="field">
-                                    <label class="label">Pavardė</label>
-                                    <div class="control">
-                                        <input class="input is-medium <?php echo isset($errors['surname']) ? 'is-danger' : '' ?>" type="text" name="surname" id="surname" placeholder="Jūsų pavardė" value="<?php echo isset($_POST['surname']) ? $_POST['surname'] : '' ?>">
-                                    </div>
-                                    <span id="surname-error"></span>
-                                    <?php
-                                    if (isset($errors['surname'])) { ?>
-                                            <span id="surname-error" class="help is-danger"><?php echo $errors['surname'] ?> </span>
-                                    <?php } ?>
-                                </div>
-                                <div class="field">
-                                    <label class="label">El. paštas</label>
-                                    <div class="control">
-                                        <input class="input is-medium <?php echo isset($errors['email']) ? 'is-danger' : '' ?>" type="email" name="email" id="email" placeholder="Jūsų el. paštas" value="<?php echo isset($_POST['email']) ? $_POST['email'] : '' ?>">
-                                    </div>
-                                    <span id="email-error"></span>
-                                    <?php
-                                    if (isset($errors['email'])) { ?>
-                                            <span id="email-error" class="help is-danger"><?php echo $errors['email'] ?></span>
-                                    <?php } ?>
-                                </div>
-                                <div class="field">
-                                    <label class="label">Slaptažodis</label>
-                                    <div class="control">
-                                        <input class="input is-medium <?php echo isset($errors['password']) ? 'is-danger' : '' ?>" type="password" name="password" id="password" placeholder="Slaptažodis" value="<?php echo isset($_POST['password']) ? $_POST['password'] : '' ?>">
-                                    </div>
-                                    <span id="password-error"></span>
-                                </div>
-                                <div class="field mb-4">
-                                    <label class="label">Slaptažodžio patvirtinimas</label>
-                                    <div class="control">
-                                        <input class="input is-medium <?php echo isset($errors['cpassword']) ? 'is-danger' : '' ?>" type="password" name="cpassword" id="cpassword" placeholder="Pakartokite slaptažodį" value="<?php echo isset($_POST['cpassword']) ? $_POST['cpassword'] : '' ?>">
-                                    </div>
-                                    <span id="cpassword-error"></span>
-                                </div>
-                                <div id="success-message" style="display:none;">Form submitted successfully!</div>
-                                <div class="field has-text-centered">
-                                    <input 
-                                        id="signup" 
-                                        class="button is-success is-fullwidth" 
-                                        type="submit" name="submit" 
-                                        value="Registruotis"
-                                    >
-                                    <p>Jau turite paskyrą?</p>
-                                    <p class="has-text-success pointer" id="signin-button">Prisijunkite</p>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                    <div class="column is-half is-hidden-touch">
-                        <img src="assets/register3.png"/>
-                    </div>
-                </div>
+  <section class="hero is-fullheight">
+    <div class="hero-body">
+      <div class="container is-widescreen">
+        <div class="columns is-centered is-tablet">
+          <div 
+            class="notification is-success has-text-centered" 
+            id="success-message" 
+            style="display:none; max-height:80px;"
+          >
+            <p>Registracija sėkminga! Netrukus galėsite prisijungti.</p>
+          </div>
+          <div 
+            class="column is-one-third" 
+            id="form-container" 
+            style="min-width:450px;"
+          >
+            <div class="container mb-4 has-text-centered">
+              <h1 class="title has-text-success" id="form-title">PRISIJUNGIMAS</h1>
             </div>
+            <div class="container is-flex-touch is-widescreen">
+              <form 
+                style="width: 100%;" 
+                id="signin-form" 
+                action="" 
+                method="post"
+              >
+                <div class="field">
+                  <label class="label">El. paštas</label>
+                  <div class="control">
+                  <input 
+                    class="input is-medium" 
+                    type="email" 
+                    name="email" 
+                    placeholder="Jūsų el. paštas" 
+                  >
+                  </div>
+                </div>
+                <div class="field">
+                  <label class="label">Slaptažodis</label>
+                  <div class="control">
+                  <input 
+                    class="input is-medium" 
+                    type="password" 
+                    name="password" 
+                    placeholder="Slaptažodis" 
+                  >  
+                  </div>
+                </div>
+                <div class="container has-text-centered">
+                  <?php
+                    if (isset($errors['signin'])) { ?>
+                      <span id="signin-error" class="has-text-danger">
+                        <?php echo $errors['signin'] ?> 
+                      </span>
+                  <?php } ?>
+                </div>
+                <div class="field has-text-centered">
+                  <button 
+                    id="signin" 
+                    class="button is-success is-fullwidth" 
+                    type="submit" name="signin" 
+                  >
+                    Prisijungti
+                  </button>
+                  <p>Neturite paskyros?</p>
+                  <p 
+                    class="has-text-success pointer" 
+                    id="signup-button"
+                  >
+                  Užsiregistruokite
+                  </p>
+                </div>
+              </form>
+              <form 
+                style="width:100%; display:none;" 
+                id="signup-form" 
+                action="register_form.php" 
+                method="post"
+              >
+                <div class="field">
+                  <label class="label">Vardas</label>
+                  <div class="control is-expanded">
+                  <input 
+                    class="input is-medium" 
+                    type="text" 
+                    name="name" 
+                    id="name" 
+                    placeholder="Jūsų vardas" 
+                  >
+                  </div>
+                  <span id="name-error"></span>
+                </div>
+                <div class="field">
+                  <label class="label">Pavardė</label>
+                  <div class="control">
+                  <input 
+                    class="input is-medium" 
+                    type="text" 
+                    name="surname" 
+                    id="surname" 
+                    placeholder="Jūsų pavardė" 
+                  >
+                  </div>
+                  <span id="surname-error"></span>
+                </div>
+                <div class="field">
+                  <label class="label">El. paštas</label>
+                  <div class="control">
+                  <input 
+                    class="input is-medium" 
+                    type="email" 
+                    name="email" 
+                    id="email" 
+                    placeholder="Jūsų el. paštas" 
+                  >
+                  </div>
+                  <span id="email-error"></span>
+                </div>
+                <div class="field">
+                  <label class="label">Slaptažodis</label>
+                  <div class="control">
+                  <input 
+                    class="input is-medium" 
+                    type="password" 
+                    name="password" 
+                    id="password" 
+                    placeholder="Slaptažodis"
+                  >
+                  </div>
+                  <span id="password-error"></span>
+                </div>
+                <div class="field mb-4">
+                  <label class="label">Slaptažodžio patvirtinimas</label>
+                  <div class="control">
+                  <input 
+                    class="input is-medium" 
+                    type="password" 
+                    name="cpassword" 
+                    id="cpassword" 
+                    placeholder="Pakartokite slaptažodį"
+                  >
+                  </div>
+                  <span id="cpassword-error"></span>
+                </div>
+                <div class="field has-text-centered">
+                  <button 
+                    id="signup" 
+                    class="button is-success is-fullwidth" 
+                    type="submit" name="submit" 
+                    value="Registruotis"
+                  >
+                    Registruotis
+                  </button>
+                  <p>Jau turite paskyrą?</p>
+                  <p 
+                    class="has-text-success pointer" 
+                    id="signin-button"
+                  >
+                    Prisijunkite
+                  </p>
+                </div>
+              </form>
+            </div>
+          </div>
+        <div class="column is-half is-hidden-touch">
+          <img src="assets/register3.png"/>
         </div>
-    </section>
-<!-- <script src="script.js"></script>     -->
-<!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
-  <!-- Include the jQuery Validate plugin -->
-<!-- <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script> -->
+      </div>
+    </div>
+  </div>
+</section>
 <script>
     var inputs = $(".input");
     inputs.on("input", function() {
